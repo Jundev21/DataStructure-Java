21cff3cd4a957b7cff7e4bae43d1a6e37cc4cb09
Jun <noah@rainbow.co.kr>
2023-06-09 18:13:57 +0900

    이진트리 설명 및 구현

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,13 +1,15 @@
 
-
-
+//트리는 그래프의 한 종류로, 다음과 같은 특징을 가지고 있습니다:
+//        사이클이 없는 연결 그래프: 트리는 사이클(cycle)이 존재하지 않는 연결 그래프입니다. 따라서 어떤 두 정점을 선택해도 정확히 하나의 경로만으로 연결되어 있습니다.
+//        단일 연결성: 트리는 한 정점에서 다른 정점으로 가는 경로가 유일합니다. 즉, 어떤 두 정점을 선택하더라도 그들을 연결하는 경로는 유일합니다.
+//        계층 구조: 트리는 계층 구조(hierarchical structure)를 가집니다. 트리의 최상위 정점을 루트(root)라고 하고, 루트에서 다른 정점들로 내려가는 경로를 따라 트리의 정점들은 계층적으로 구성됩니다.
+//        트리는 이러한 특징으로 인해 그래프의 일종으로 분류됩니다. 그러나 일반적인 그래프와는 다르게 특별한 제약 조건을 가지고 있어 계층 구조를 표현하거나 계층적인 데이터를 표현하는 데 유용합니다. 트리는 컴퓨터 과학에서 다양한 분야에서 활발하게 활용되며, 예를 들어 파일 시스템, 데이터베이스 인덱스, 알고리즘 등에서 사용됩니다.
 
 public class BinaryTree {
 
 
     static public class Node{
         int data = 0;
-
         Node left;
         Node right;
         Node(int data){
@@ -46,11 +48,9 @@ public class BinaryTree {
 
             inorder(root.right);
 
-
         }
 
         static void postOrder(Node root){
-
 
             if(root == null) return;
 
@@ -66,7 +66,6 @@ public class BinaryTree {
     public static void main(String[] args) {
 
         BinaryTree.Node rootNode = new Node(1);
-
 
         rootNode.left = new Node(2);
 
