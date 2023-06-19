ef161b422272bfe6bad4514b83246932e37a26a4
Jun <noah@rainbow.co.kr>
2023-06-19 18:32:08 +0900

    링크드리스트 내용

--- src/LinkedLista.java
+++ src/LinkedLista.java
@@ -2,14 +2,11 @@
 
 
 // 링크드 리스트 와 어레이 리스트의 차이점.
-
 // 링크드 리스트와 어레이 리스트의 공통점 부터 보자면 선형주조로 되어있는 자료구조이며 배열 과는 다르게 데이터를 중간에 추가하거나 삭제가 가능하다.
-
 // 차이점을보자면 링크드 리스트는 각 데이터의 주소들로 연결되어있어 데이터를 추가 삭제할때 빠르고 편리하다는 장점이있다
-
-// 반면에 링크드 리스트는 데이터 추가 삭제가 가능은 하지만 배열을 기반으로 만들어져있어 링크드 리스트에 비해서는 비효율적이지만
-
+// 반면에 어레이 리스트는 데이터 추가 삭제가 가능은 하지만 배열을 기반으로 만들어져있어 링크드 리스트에 비해서는 비효율적이지만
 // 데이터를 찾을때는 인덱스를 통해서 찾을 수 있기 때문에 이부분에서는 링크드 리스트 에 비해 빠르다.
+
 
 
 public class LinkedLista {
@@ -19,13 +16,11 @@ public class LinkedLista {
 //        list 클래스에는 노드 정보를 담고있어야한다.
 //        노드 정보에는 현재 데이터 그리고 어디를 가리키고 있는지에 대한 정보와 다음 어디를 가르키고
 //        있는지 head 에 대한 정보 필요
-
         private NodeInfo head =null;
         private NodeInfo tail =null;
         private int size =0;
 
         private class NodeInfo{
-
             private int data =0;
             private NodeInfo nextNode;
 
@@ -42,7 +37,6 @@ public class LinkedLista {
                 return String.valueOf(this.data);
             }
         }
-
 
         public void insertToFirstNode(int data){
 
